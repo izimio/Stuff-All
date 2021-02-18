2f49e38bc449e93eb8e6670b71fbe82bc161edbc
Joshua Brionne <Joshua.brionne@gmail.com>
2021-02-18 22:59:12 +0100

    v2

---
+++ Hang_man/Hang_man_V2/function.c
@@ -0,0 +1,76 @@
+
+# include <stdlib.h>
+# include <stdio.h>
+#include <fcntl.h>
+
+int strlen(char *str)
+{
+    int i = 0;
+    while (str[i])
+    {
+        i++;
+    }
+    return(i);
+}
+
+int Getlines()
+{
+    int file;
+    int i;
+    char j;
+
+    i = 0;
+    j = '0';
+    file = open("fr.txt", O_RDONLY);
+    while (read(file, &j, 1))
+    {
+        if(j == '\n')
+            i++;
+    }
+    return(i);
+}
+
+char *giveWord(int a)
+{
+    int file;
+    int d;
+    char j;
+    int i;
+    int lenght;
+    char *str;
+
+    i = 0;
+    lenght = 1;
+    j = '0';
+    d = -1;
+    file = open("fr.txt", O_RDONLY);
+
+    while (i != a)
+    {
+        read(file, &j, 1);
+        if (j == '\n')
+            i++;
+    }
+    read(file, &j, 1);
+    while (j != '\n')
+    {
+        read(file, &j, 1);
+        lenght++;
+    }
+    i = 0;
+    str = (char *)malloc(sizeof(char) * lenght + 1);
+    while (i != a)
+    {
+        read(file, &j, 1);
+        if (j == '\n')
+            i++;
+    }
+    read(file, &j, 1);
+    while (j != '\n')
+    {
+        str[++d] = j;
+        read(file, &j, 1);
+    }
+    str[++d] = '\0';
+    return(str);
+}
---
+++ Hang_man/Hang_man_V2/random_hang_man.c
@@ -0,0 +1,128 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int strlen(char *str);
+char *giveWord(int a);
+int Getlines();
+
+void my_putchar(char c)
+{
+    printf("%c", c);
+}
+
+void giveRandomletters(int tab[], int size)
+{
+    int i;
+
+    i = -1;
+    while (++i < size)
+    {
+        if (i % 3 == 0)
+            tab[i] = 1;
+    }
+}
+
+void writeStr(char *str)
+{
+    int i;
+
+    i = -1;
+    while (str[++i])
+        my_putchar(str[i]);
+}
+
+void print_result(int tab[], char *str)
+{
+    int i;
+
+    i = -1;
+    while (str[++i])
+        if (tab[i] == 0)
+            writeStr(" - ");
+        else if (tab[i] == 1)
+            my_putchar(str[i]);
+    my_putchar('\n');
+}
+
+int verifInput(int tab[], char *str, char c)
+{
+    int i;
+    int tmp;
+
+    tmp = 0;
+    i = -1;
+    while (str[++i])
+        if (c == str[i] && tab[i] == 0)
+        {
+            tab[i] = 1;
+            tmp = 1;
+        }
+    return (tmp);
+}
+
+int verifTab(int tab[], int length)
+{
+    int i;
+
+    i = -1;
+    while (++i < length)
+        if (tab[i] == 0)
+            return (0);
+    return (1);
+}
+
+int *createArr(char *str)
+{
+    int *arr;
+    int i;
+    int length = strlen(str);
+
+    i = 0;
+    arr = (int *)malloc(sizeof(int) * length);
+    while (i < length)
+        arr[i++] = 0;
+    return (arr);
+}
+
+int main(int ac, char *argv[])
+{
+    srand(time(NULL));
+    char *str;
+    int lives;
+    int *arr;
+    char tmp;
+    int length;
+    int ui;
+    int get;
+
+    get = Getlines();
+    ui = rand() % get;
+
+    lives = 10;
+    str = giveWord(ui);
+    length = strlen(str);
+    arr = createArr(str);
+    giveRandomletters(arr, length);
+    writeStr("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    print_result(arr, str);
+
+    while (lives > 0 && verifTab(arr, length) == 0)
+    {
+        printf("\nEntrez une lettre\nNombre de vies : %d\n", lives / 2);
+        tmp = getchar();
+        if (verifInput(arr, str, tmp) == 1)
+            lives++;
+        else
+            lives--;
+        print_result(arr, str);
+    }
+    if (verifTab(arr, length) == 1)
+        writeStr("\n\n    ---------\n    | Gagne |\n    ---------");
+    if (lives <= 0)
+    {
+        writeStr("\n\n");
+        writeStr(str);
+        writeStr("\n    ---------\n    | Perdu |\n    ---------");
+    }
+}
